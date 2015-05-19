58cce050ac20f099f170c3c208f431014adafe77
Anibal Gomez <anibalgomez@balerocms.com>
2015-05-19 10:00:56 -0500

    Password Encryption Unit Test

---
+++ src/test/java/com/neblina/balero/service/PasswordServiceTest.java
@@ -0,0 +1,32 @@
+/**
+ * Prueba unitaria creada por:  Anibal Gomez <anibalgomez@icloud.com>
+ */
+
+package com.neblina.balero.service;
+
+import com.neblina.balero.Application;
+import com.neblina.balero.util.PasswordService;
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationContextLoader;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ActiveProfiles("dev")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Application.class, loader = SpringApplicationContextLoader.class)
+public class PasswordServiceTest extends TestCase {
+
+    @Test
+    public void generatePassword() throws Exception {
+        try {
+            System.out.println("Generando password SHA1...");
+            System.out.println(PasswordService.getInstance().encrypt("password"));
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+}
