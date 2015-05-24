cf7b22b0a60a5d672e56b966ffe745dd56fd2ec9
Anibal Gomez <anibalgomez@balerocms.com>
2015-05-24 11:38:45 -0500

    Offline Template

---
+++ src/main/java/com/neblina/balero/web/PagesController.java
@@ -0,0 +1,32 @@
+/**
+ * Silbato Project: Proyecto 100% Mexicano de código libre.
+ *
+ * @author      Anibal Gomez <anibalgomez@icloud.com>
+ * @copyright   Copyright (C) 2015 Neblina Software. Derechos reservados.
+ * @license     Licencia Pública GNU versión 3 o superior; vea LICENSE.txt
+ */
+
+package com.neblina.balero.web;
+
+import com.neblina.balero.model.SettingsModel;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PagesController {
+
+    private static final Logger log = LogManager.getLogger(TestController.class.getName());
+
+    @Autowired
+    private SettingsModel settingsModel;
+
+    @RequestMapping("/offline")
+    String offline(Model model) {
+        return "offline";
+    }
+
+}
