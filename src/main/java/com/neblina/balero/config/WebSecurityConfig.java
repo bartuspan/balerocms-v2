5445c82b116fc63e38c1b4ff332e9ce68fcf3d4c
Anibal Gomez <anibalgomez@balerocms.com>
2015-05-25 18:12:52 -0500

    Unused Line

--- src/main/java/com/neblina/balero/config/WebSecurityConfig.java
+++ src/main/java/com/neblina/balero/config/WebSecurityConfig.java
@@ -8,10 +8,8 @@
 
 package com.neblina.balero.config;
 
-import com.neblina.balero.service.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -28,9 +26,6 @@ import java.util.Properties;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final Logger log = LoggerFactory.getLogger(WebSecurityConfig.class);
-
-    @Autowired
-    private UserRepository userRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
