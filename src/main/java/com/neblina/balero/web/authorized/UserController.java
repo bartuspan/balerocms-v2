6f0608bccd4ca44c6686372f1030602de0aeae33
Anibal Gomez <anibalgomez@balerocms.com>
2015-05-26 13:09:06 -0500

    Register Template

--- src/main/java/com/neblina/balero/web/authorized/UserController.java
+++ src/main/java/com/neblina/balero/web/authorized/UserController.java
@@ -1,5 +1,5 @@
 /**
- * Silbato Project: Proyecto 100% Mexicano de código libre.
+ * Balero CMS Project: Proyecto 100% Mexicano de código libre.
  *
  * @author      Anibal Gomez <anibalgomez@icloud.com>
  * @copyright   Copyright (C) 2015 Neblina Software. Derechos reservados.
@@ -8,6 +8,7 @@
 
 package com.neblina.balero.web.authorized;
 
+import com.neblina.balero.model.SettingsModel;
 import com.neblina.balero.service.UserService;
 import com.neblina.balero.service.repository.UserRepository;
 import com.neblina.balero.util.PasswordGenerator;
@@ -19,6 +20,7 @@ import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,12 +44,21 @@ public class UserController {
 
     private static final Logger log = LogManager.getLogger(UserController.class.getName());
 
+    @Autowired
+    private SettingsModel settingsModel;
+
     @RequestMapping( value = {"", "/"} )
     public String rootIndex() {
         return "redirect:/user/dashboard";
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String registerForm(Model model) {
+        model.addAllAttributes(settingsModel.add());
+        return "silbato/register";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
     public String register(@RequestParam("username") String username) {
         PasswordGenerator pwd = new PasswordGenerator();
         userService.createUserAccount("demo", "123456", "Pepito", "Perez", "demo@localhost", "USER");
